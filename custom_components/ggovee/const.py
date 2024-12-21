7876ab2bf19f9b7288f4cb06d6db096000a955ae
Emil Doležal <emil.dolezal@cts-tradeit.cz>
2024-12-21 19:33:53 +0100

    hgfh

--- custom_components/ggovee/const.py
+++ custom_components/ggovee/const.py
@@ -1,5 +1,21 @@
-"""Constants for the ggovee integration."""
-import logging
+"""Constants for the Govee integration."""
+from homeassistant.const import TEMP_CELSIUS, Platform
 
 DOMAIN = "ggovee"
-LOGGER = logging.getLogger(__package__)
+CONF_API_KEY = "api_key"
+CONF_SERVER_REGION = "server_region"
+DEFAULT_SERVER_REGION = "us"
+SUPPORTED_REGIONS = ["us", "eu"]
+
+STORAGE_KEY = "ggovee_data"
+STORAGE_VERSION = 1
+
+MANUFACTURER = "Govee"
+BRAND = "Govee"
+
+UPDATE_INTERVAL = 300
+
+# Tady doplňujeme PLATFORMS,
+# podle toho, jaké platformy váš kód používá.
+# V repozitáři vidím pouze `sensor.py`, takže:
+PLATFORMS = [Platform.SENSOR]
