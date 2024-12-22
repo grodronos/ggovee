1c4381656a304d7f98b8204a92ee8f2601179a0b
Emil Doležal <emil.dolezal@cts-tradeit.cz>
2024-12-22 20:14:40 +0100

    Test

--- custom_components/ggovee/__init__.py
+++ custom_components/ggovee/__init__.py
@@ -1,18 +1,35 @@
 """Support for Govee bluetooth sensors."""
-import logging
-
+from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
+from .GoveeApi.UserDevices.controller import Controller
+from .GoveeApi.UserDevices.models import Device
 from .const import DOMAIN, STARTUP_MESSAGE
 from .coordinator import GoveeDataUpdateCoordinator
+from typing import Final
+import logging
+import asyncio
 
-_LOGGER = logging.getLogger(__name__)
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Govee from a config entry."""
-    _LOGGER.info("__init__:async_setup_entry")
+
+    _LOGGER.debug("grodronos")
+    controller = Controller(api_key=str(entry.data.get(CONF_API_KEY, None)))
+
+    try:
+        # Získání zařízení
+        devices: List[Device] = await controller.getDevices(hass)
+         # Výpis zařízení
+        print(f"Získáno zařízení: {len(devices)}")
+        for device in devices:
+            print(f"Zařízení: {device.deviceName} ({device.device})")
+            for capability in device.capabilities:
+                print(f"  - {capability.instance}")
+    except Exception as e:
+        print(f"Chyba: {e}")
 
     # Vytvoříme instanci koordinátoru:
     coordinator = GoveeDataUpdateCoordinator(hass, entry)
@@ -25,19 +42,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if coordinator.data:
         device = coordinator.data.get("device")
 
-    # Uložíme data do hass.data - sem si pak "sáhne" platforma sensor.py nebo climate.py
+    # Uložíme data do hass.data - sem si pak "sáhne" platforma sensor.py
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = {
         "coordinator": coordinator,
         "device": device,  # Ukládáme device, aby byl k dispozici v sensor.py i jinde
     }
 
-    # Dopředné nastavení platformy sensor (to samé pro climate)
+    # Dopředné nastavení platformy sensor
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "climate")
     )
 
     return True
@@ -45,7 +59,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Govee config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor", "climate"])
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
