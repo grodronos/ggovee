52ef3312339ec60146efc94b1fa3fc908807ac86
Emil Doležal <emil.dolezal@cts-tradeit.cz>
2024-12-28 13:21:11 +0100

    Obsolete code replaced

--- custom_components/ggovee/__init__.py
+++ custom_components/ggovee/__init__.py
@@ -1,4 +1,3 @@
-"""Support for Govee bluetooth sensors."""
 from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -15,7 +14,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
-    hass.async_add_job(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
